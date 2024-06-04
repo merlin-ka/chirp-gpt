592d005bdc5dd50871e671eaf4ddcc43949ca79c
Merlin K. <merlinkannengiesser@gmail.com>
2024-06-04 20:40:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chirp-GPT
+
+Make Chat-GPT talk with a high pitched voice as it's typing
+
+## Installation
+
+(todo)
+
+**THIS PROJECT DOES NOT USE ANY COPYRIGHTED MATERIAL. ALL SOUNDS WERE RECORDED BY MYSELF.**
---
+++ script.js
@@ -0,0 +1,128 @@
+// ==UserScript==
+// @name         Chirp-GPT
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Make Chat-GPT talk with a funny voice.
+// @author       merlin-ka (github.com/merlin-ka)
+// @match        https://chatgpt.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
+// @grant        none
+// ==/UserScript==
+
+const PLAY_INTERVAL = 80;
+const DETUNE = 500;
+
+const audioContext = new AudioContext();
+const gain = audioContext.createGain();
+gain.gain.value = 0;
+
+// if the script fails to add the UI elements, set this to true to stop it from trying
+let hasCreateUiFailed = false;
+
+function createUi() {
+    const header = document.querySelector("main div.sticky");
+
+    if (!header) {
+        hasCreateUiFailed = true;
+        return;
+    }
+
+    const accountButton = header.children[header.children.length - 1];
+
+    if (!accountButton) {
+        hasCreateUiFailed = true;
+        return;
+    }
+
+    const div = document.createElement("div");
+    div.id = "chirp-gpt-ui";
+    div.className = "flex items-center";
+
+    const label = document.createElement("div");
+    label.innerText = "Chirp-GPT Volume:";
+    label.style.marginRight = "16px";
+    div.appendChild(label);
+
+    const slider = document.createElement("input");
+    slider.type = "range";
+    slider.value = Math.round(gain.gain.value * 100);
+    div.appendChild(slider);
+
+    slider.addEventListener("input", (e) => {
+        gain.gain.value = slider.valueAsNumber;
+    });
+
+    accountButton.insertAdjacentElement("beforebegin", div);
+}
+
+async function createAudioBuffer(waveBase64) {
+    const byteString = atob(waveBase64);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const byteArray = new Uint8Array(arrayBuffer);
+
+    for (let i = 0; i < byteString.length; i++) {
+        byteArray[i] = byteString.charCodeAt(i);
+    }
+
+    return await audioContext.decodeAudioData(arrayBuffer);
+}
+
+function playAudioBuffer(audioBuffer) {
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.detune.value = Math.round(Math.random() * DETUNE);
+    source.connect(audioContext.destination);
+    source.start();
+}
+
+async function main() {
+    // const sounds = wavFiles.map(createAudioBuffer);
+
+    // Play a sound when text is added to the response (detected by the length)
+    let previousResponseLength = 0;
+
+    // Timestamp in milliseconds of the last time a sound was played
+    let previousPlayTime = 0;
+
+    const observer = new MutationObserver(() => {
+        // first, check if the UI is visible
+        // (which is not the case on page load and chat navigation)
+        if (!hasCreateUiFailed && document.querySelector("#chirp-gpt-ui") == null) {
+            createUi();
+        }
+
+        if (gain.gain.value == 0) {
+            return;
+        }
+
+        /** @type {HTMLDivElement} */
+        const resultStreamingDiv = document.querySelector("div.result-streaming");
+
+        if (!resultStreamingDiv) {
+            previousResponseLength = 0;
+            return;
+        }
+
+        // check for new content
+        if (resultStreamingDiv.innerText.length == previousResponseLength) {
+            return;
+        }
+        previousResponseLength = resultStreamingDiv.innerText.length;
+
+        // prevent sounds from overlapping too much
+        const now = Date.now();
+        if (now - previousPlayTime < PLAY_INTERVAL) {
+            return;
+        }
+        previousPlayTime = now;
+
+        const idx = Math.round(Math.random() * (sounds.length - 1));
+        sounds[idx].play();
+    });
+
+    observer.observe(document.body, { characterData: true, childList: true, subtree: true });
+}
+
+(function () {
+    main();
+})();
